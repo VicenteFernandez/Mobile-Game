a5ec1805210477f3a5f7c00290913efdf19f2e27
Ethan5899 <35109417+Ethan5899@users.noreply.github.com>
2018-01-30 10:10:25 -0800

    added some basic player controls -EP

---
+++ MobileProject/Assets/Scripts/CameraMove.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour {
+    public GameObject Player;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(Player.transform.position.x, Player.transform.position.y, transform.position.z);
+    }
+
+}
---
+++ MobileProject/Assets/Scripts/PlayerControler.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControler : MonoBehaviour {
+    public float moveSpeed = 1, jumpSpeed = 1, bounceSpeed = 1;
+    public bool grounded = false, canBounce = false;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Player movement left and right
+        float moveX = Input.GetAxis("Horizontal");
+        Vector2 velocity = GetComponent<Rigidbody2D>().velocity;
+        velocity.x = moveX * moveSpeed;
+        GetComponent<Rigidbody2D>().velocity = velocity;
+
+        //jump
+        if(Input.GetButtonDown("Jump") && grounded == true)
+        {
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 100 * jumpSpeed));
+           
+        }
+
+    }
+
+    public void Grounded()
+    {
+        grounded = true;
+    }
+    public void NotGrounded()
+    {
+        grounded = false;
+    }
+}
