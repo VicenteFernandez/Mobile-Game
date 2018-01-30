e78c5a4eebfb523bf91f601f36f266e6bd7b6c0e
Ethan5899 <35109417+Ethan5899@users.noreply.github.com>
2018-01-30 10:29:21 -0800

    EP

---
+++ MobileProject/Assets/Scripts/EnemyMove.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMove : MonoBehaviour {
+    public float moveSpeed = 1, triggerDistance = 10;
+    public GameObject player;
+    bool on = false, moveRight = false, moveLeft = false;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 playerPos = player.transform.position;
+        Vector2 Distance = new Vector2(playerPos.x - transform.position.x,
+                                                playerPos.y - transform.position.y);
+        if (Distance.magnitude < triggerDistance)
+        {
+            on = true;
+
+        }
+        Vector2 velocity = GetComponent<Rigidbody2D>().velocity;
+        if (on == true && moveRight == false)
+        {
+            velocity.x = moveSpeed * -1;
+            GetComponent<Rigidbody2D>().velocity = velocity;
+            moveLeft = true;
+
+        }
+        if (on == true && moveRight == true)
+        {
+            velocity.x = moveSpeed * 1;
+            GetComponent<Rigidbody2D>().velocity = velocity;
+
+        }
+    }
+    public void HitWall()
+    {
+        Debug.Log("HitWall");
+        if (moveRight == false && moveLeft == true)
+        {
+            moveRight = true;
+            moveLeft = false;
+            return;
+        }
+        if (moveLeft == false && moveRight == true)
+        {
+            moveLeft = true;
+            moveRight = false;
+            return;
+        }
+    }
+}
---
+++ MobileProject/Assets/Scripts/EnemyWallDetector.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyWallDetector : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.layer == 8)
+        {
+            GetComponentInParent<EnemyMove>().HitWall();
+        }
+    }
+    private void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.gameObject.layer == 8)
+        {
+            GetComponentInParent<EnemyMove>().HitWall();
+        }
+    }
+}
