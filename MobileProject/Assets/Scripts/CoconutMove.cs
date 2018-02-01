71d6cf649158dc40218fa7ed0f54961d7f6786b7
Ethan5899 <35109417+Ethan5899@users.noreply.github.com>
2018-02-01 10:07:38 -0800

    created death scenes and made the coconut enemy -EP

---
+++ MobileProject/Assets/Scripts/CoconutMove.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoconutMove : MonoBehaviour {
+    public float moveSpeed = 1, triggerDistance = 10;
+    public GameObject Player;
+    private bool on = false, grounded = false;
+    public string moveDirection = "left";
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector2 playerPos = (Player.transform.position);
+        Vector2 distance = new Vector2 (playerPos.x - transform.position.x, playerPos.y - transform.position.y);
+        if(distance.magnitude <= triggerDistance)
+        {
+            on = true;
+        }
+        Vector2 velocity = GetComponent<Rigidbody2D>().velocity;
+        if ( on == true && grounded == true && moveDirection == "left")
+        {
+            velocity.x = -1 * moveSpeed;
+            GetComponent<Rigidbody2D>().velocity = velocity;
+
+        }
+        if (on == true && grounded == true && moveDirection == "right")
+        {
+            velocity.x = 1 * moveSpeed;
+            GetComponent<Rigidbody2D>().velocity = velocity;
+
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.layer == 8)
+        {
+            grounded = true;
+        }
+        else
+        {
+            grounded = false;
+        }
+    }
+}
---
+++ MobileProject/Assets/Scripts/DeathScene.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeathScene : MonoBehaviour {
+    float timer = 0;
+    public float limit = 3;
+    public string SceneToLoad;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer += Time.deltaTime;
+        if(timer >= limit)
+        {
+            SceneManager.LoadScene(SceneToLoad);
+        }
+	}
+}
