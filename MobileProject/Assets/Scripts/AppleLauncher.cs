8d4c87909596e0f2e9163c66d5714ff8bc530399
Ethan5899 <35109417+Ethan5899@users.noreply.github.com>
2018-02-05 10:09:24 -0800

    roasted pig EP

---
+++ MobileProject/Assets/Scripts/AppleLauncher.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppleLauncher : MonoBehaviour {
+    public GameObject prefab;
+    bool done = false;
+    float timer = 0;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(done == true)
+        {
+            timer += Time.deltaTime;
+            if (timer >= 2)
+            {
+               
+                done = false;
+            }
+        }
+	}
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player" && done == false)
+        {
+            timer = 0;
+            done = true;
+            Vector3 spawnPos = gameObject.transform.position;
+            GameObject Coconut = (GameObject)Instantiate(prefab, spawnPos, Quaternion.identity);
+        }
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player" && done == false)
+        {
+            timer = 0;
+            done = true;
+            Vector3 spawnPos = gameObject.transform.position;
+            GameObject Coconut = (GameObject)Instantiate(prefab, spawnPos, Quaternion.identity);
+        }
+    }
+}
---
+++ MobileProject/Assets/Scripts/AppleMove.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppleMove : MonoBehaviour {
+    public float moveSpeed = 3;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector2 velocity = GetComponent<Rigidbody2D>().velocity;
+        velocity = new Vector2(-1 * moveSpeed, 0);
+        GetComponent<Rigidbody2D>().velocity = velocity;
+    }
+}
