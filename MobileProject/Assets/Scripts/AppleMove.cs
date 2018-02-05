a581a4ba5cf8d50772964f6769cba4e886ea487d
Ethan5899 <35109417+Ethan5899@users.noreply.github.com>
2018-02-05 10:27:54 -0800

    EP

--- MobileProject/Assets/Scripts/AppleMove.cs
+++ MobileProject/Assets/Scripts/AppleMove.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class AppleMove : MonoBehaviour {
     public float moveSpeed = 3;
+    float timer = 0;
 	// Use this for initialization
 	void Start () {
 		
@@ -14,5 +15,10 @@ public class AppleMove : MonoBehaviour {
         Vector2 velocity = GetComponent<Rigidbody2D>().velocity;
         velocity = new Vector2(-1 * moveSpeed, 0);
         GetComponent<Rigidbody2D>().velocity = velocity;
+        timer += Time.deltaTime;
+        if (timer >= 4)
+        {
+            Destroy(gameObject);
+        }
     }
 }
