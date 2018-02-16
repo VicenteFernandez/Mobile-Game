73bcf25c1423ebfe6ccb26d6bdbbc873d34a1508
Ethan5899 <35109417+Ethan5899@users.noreply.github.com>
2018-02-16 10:08:52 -0800

    worked on level1 -EP

---
+++ MobileProject/Assets/Scripts/LoadNextLevel.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadNextLevel : MonoBehaviour {
+    public string NextLevelName = "SceneNameHere";
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "LevelLoader")
+        {
+            SceneManager.LoadScene(NextLevelName);
+        }
+    }
+}
--- MobileProject/Assets/Scripts/Raft.cs
+++ MobileProject/Assets/Scripts/Raft.cs
@@ -5,9 +5,13 @@ using UnityEngine;
 public class Raft : MonoBehaviour {
     bool on = false;
     public float speed = 1;
+    float timer = 0;
+    public float resetTimer = 15;
+    Vector3 pos;
+    bool reset = false;
 	// Use this for initialization
 	void Start () {
-		
+       pos = new Vector3 (GetComponent<Transform>().position.x, GetComponent<Rigidbody2D>().position.y);
 	}
 	
 	// Update is called once per frame
@@ -16,12 +20,26 @@ public class Raft : MonoBehaviour {
         {
             GetComponent<Rigidbody2D>().velocity = new Vector2(speed, 0);
         }
+        if(reset == true)
+        {
+            timer += Time.deltaTime;
+        }
+        if (timer >= resetTimer)
+        {
+            timer = 0;
+            GetComponent<Transform>().position = pos;
+            reset = false;
+        }
 	}
     private void OnCollisionEnter2D(Collision2D col)
     {
         if(col.gameObject.tag == "Player")
         {
             on = true;
+        }
+        if(col.gameObject.layer == 8 && on == true)
+        {
+            reset = true;
         }
     }
 }
