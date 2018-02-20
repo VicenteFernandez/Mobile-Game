4cffdbba9ed5cd612f8c62f1a39f0792dd67d722
Ethan5899 <35109417+Ethan5899@users.noreply.github.com>
2018-02-20 09:48:22 -0800

    EP

--- MobileProject/Assets/Scripts/PlayerControler.cs
+++ MobileProject/Assets/Scripts/PlayerControler.cs
@@ -17,9 +17,9 @@ public class PlayerControler : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         //Player movement left and right
-            moveX = Input.GetAxis("Horizontal");
+        moveX = Input.GetAxis("Horizontal");
                
-        //moveX = CrossPlatformInputManager.GetAxis("Horizontal");
+        moveX = CrossPlatformInputManager.GetAxis("Horizontal");
         Vector2 velocity = GetComponent<Rigidbody2D>().velocity;
         if (onIce == false && canMove == true)
         {
