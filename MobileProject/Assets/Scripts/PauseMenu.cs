47553a8ffdd03e69d41a8b8418d6bc84d7b4d382
Ethan5899 <35109417+Ethan5899@users.noreply.github.com>
2018-02-21 09:14:07 -0800

    EP

--- MobileProject/Assets/Scripts/PauseMenu.cs
+++ MobileProject/Assets/Scripts/PauseMenu.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityStandardAssets.CrossPlatformInput;
 public class PauseMenu : MonoBehaviour {
     //attach this script to both Canvases
     public GameObject pauseCanvas;
     public GameObject notPausedCanvas;
     public string currentLevel;
+    float buttonPressed = 0;
 	// Use this for initialization
 	void Start () {
         pauseCanvas.GetComponent<Canvas>().enabled = false;
@@ -15,25 +17,49 @@ public class PauseMenu : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        buttonPressed = CrossPlatformInputManager.GetAxis("Fire2");
+        if(buttonPressed == 1)
+        {
+            buttonPressed = 0;
+            PauseGame();
+        }
+        if (buttonPressed == 2)
+        {
+            
+            buttonPressed = 0;
+            Resume();
+        }
+        if (buttonPressed == 3)
+        {
+            MainMenu();
+        }
+        if (buttonPressed == 4)
+        {
+            Restart();
+        }
+    }
+    
     public void PauseGame()
     {
         Time.timeScale = 0;
         pauseCanvas.GetComponent<Canvas>().enabled = true;
+        
     }
     public void Resume()
     {
         Time.timeScale = 1;
         pauseCanvas.GetComponent<Canvas>().enabled = false;
+        
     }
     public void Restart()
     {
         
         SceneManager.LoadScene(currentLevel);
+        
     }
     public void MainMenu()
     {
         SceneManager.LoadScene("MainMenu");
+      
     }
 }
---
+++ MobileProject/Assets/Scripts/TouchButtons.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchButtons : MonoBehaviour {
+    
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            Debug.Log("Success");
+        }
+	}
+}
