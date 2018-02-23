8e1790220de5a738bb2775890ca977dbb46c00e6
Ethan5899 <35109417+Ethan5899@users.noreply.github.com>
2018-02-23 10:01:11 -0800

    EP

---
+++ MobileProject/Assets/Scripts/fireTree.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fireTree : MonoBehaviour {
+    public GameObject prefab;
+    private bool done = false;
+    public float timer;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(done == true)
+        {
+            timer += Time.deltaTime;
+            if(timer >= 10)
+            {
+                timer = 0;
+                done = false;
+            }
+        }
+
+    }
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (done == false)
+        {
+            done = true;
+            Vector3 spawnPos = gameObject.transform.position;
+            GameObject Coconut = (GameObject)Instantiate(prefab, spawnPos, Quaternion.identity);
+
+        }
+
+    }
+    private void OnTriggerStay2D(Collider2D col)
+    {
+        if (done == false)
+        {
+            done = true;
+            Vector3 spawnPos = gameObject.transform.position;
+            GameObject Coconut = (GameObject)Instantiate(prefab, spawnPos, Quaternion.identity);
+
+        }
+
+    }
+
+
+}
